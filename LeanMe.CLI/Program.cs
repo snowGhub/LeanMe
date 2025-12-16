7ffc1d676ad9f4cf1f20b782cfc41dcee16ca24a
Rudolf Astl <rudolf@astl.cc>
2025-12-16 08:58:15 +0100

    added Programm Test Call in the CLI

--- LeanMe.CLI/Program.cs
+++ LeanMe.CLI/Program.cs
@@ -1,3 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
+ 
+using LeanMe.LibaryManager;
 
-Console.WriteLine("Hello, World!");
+
+var service = new LibaryService();
+
+Console.WriteLine("=== Lean Console App by Rudolf Astl===");
+
+service.AddMedium(new Book(1,"Clean Code", 2,"Rudolf Astl","ISBN-PLACEHOLDER"));
+service.AddMedium(new DVD(2, "Inception", 1,"Rudolf Astl der 2.",140));
+
+service.AddCostumer(new Customer(100, "Rudolf Astl der 3.","Musterstraße 1"));
+service.AddCostumer(new Customer(101, "Rudolf Astl der 3.", "Musterweg 2"));
+
+Console.WriteLine("=== Medien ===");
+foreach (var variable in service.ShowMedia())
+{
+    Console.WriteLine(variable);
+}
+
+foreach (var variables in service.ShowCustomers())
+{
+    Console.WriteLine(variables);
+}
